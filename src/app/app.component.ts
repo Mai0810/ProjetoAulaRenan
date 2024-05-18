1d1ce2ccd6ded6490348c8a0a7f5cdde5edcb2a8
LeonardoRk <maizers08@gmail.com>
2024-05-18 18:19:39 -0300

    adicionei admin, produtor logado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { PortfolioItemComponent } from './portfolio-item/portfolio-item.componen
 import { CadastroFormComponent } from './cadastro-form/cadastro-form.component';
 import { ModalProdutoComponent } from './modal-produto/modal-produto.component';
 import { TabelaDoProdutorComponent } from './tabela-do-produtor/tabela-do-produtor.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProdutorComponent } from './produtor-logado/produtor-logado.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, PortfolioItemComponent, CadastroFormComponent, TabelaDoProdutorComponent ],
+  imports: [RouterOutlet, LoginComponent, PortfolioItemComponent, CadastroFormComponent, TabelaDoProdutorComponent, AdminComponent, ProdutorComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
