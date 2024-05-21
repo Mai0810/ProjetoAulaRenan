06c8980a0fb6338be2f2823d330e221d2b12f153
LeonardoRk <maizers08@gmail.com>
2024-05-21 18:34:42 -0300

    login e usuario

---
+++ src/app/dto/usuario.model.ts
@@ -0,0 +1,24 @@
+export class Usuario {
+    ID_USUARIO: number = null as unknown as number;
+    NOME: string = null as unknown as string;
+    EMAIL: string = null as unknown as string;
+    SENHA: string = null as unknown as string;
+    ADMIN: number = null as unknown as number;
+
+    constructor(data?: any) {
+        if (data) {
+            this.ID_USUARIO = data.ID_USUARIO;
+            this.NOME = data.NOME;
+            this.EMAIL = data.EMAIL;
+            this.SENHA = data.SENHA;
+            this.ADMIN = data.ADMIN;
+        }
+    }
+
+    public isAdmin(): boolean {
+        if(this.ADMIN && this.ADMIN === 1){
+            return true;
+        }
+        return false;
+    } 
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,61 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginService } from '../services/login.service';
+import { HttpClient, HttpClientModule, HttpStatusCode } from '@angular/common/http';
+import { Usuario } from '../dto/usuario.model';
+import { Router } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    ReactiveFormsModule],
+    ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  form: FormGroup | null = null; // Definindo o tipo da variável 'form' como FormGroup
+  form: FormGroup; // Definindo o tipo da variável 'form' como FormGroup
 
-    constructor(private formBuilder: FormBuilder) {
-        this.criarForm();
+    constructor(private formBuilder: FormBuilder, private loginService:LoginService, private router: Router) {
+      this.form = this.formBuilder.group({
+          email: ['', [Validators.required]],
+          senha: ['', [Validators.required]]
+      });
     }
 
-    criarForm() {
-        this.form = this.formBuilder.group({
-            email: [''],
-            senha: ['']
+    private lidaComUsuario(usuario:Usuario) {
+      if(!usuario.ID_USUARIO){
+        alert("Email e/ou senha inválidos");
+      }
+      console.log(usuario);
+      if(usuario.isAdmin()){
+        // vai pro admin
+        this.router.navigate(['/admin']);
+      }else {
+        // vai pro produtor
+        this.router.navigate(['/produtor-logado']);
+      }
+    }
+
+    submeterLogin(){
+      if(this.form?.valid){
+        const email :string =  this.form.get('email')?.value;
+        const senha: string = this.form.get('senha')?.value;
+        this.loginService.login(email, senha).subscribe({
+          next: (usuario: Usuario) => {
+            this.lidaComUsuario(usuario);
+          },
+          error: (httpResponse) => {
+            if(httpResponse.status == HttpStatusCode.NotFound){
+              alert(httpResponse.error.msg);
+            }
+          }
         });
+      }else {
+        alert("formulário inválido de login");
+      }
     }
   
 }
