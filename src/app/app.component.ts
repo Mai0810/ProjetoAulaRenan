8efbbd7195daccc2b0273cf603c43c04f49c27dc
LeonardoRk <maizers08@gmail.com>
2024-05-21 13:06:58 -0300

    front app.component html e ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { PortfolioItemComponent } from './portfolio-item/portfolio-item.component';
-import { CadastroFormComponent } from './cadastro-form/cadastro-form.component';
-import { ModalProdutoComponent } from './modal-produto/modal-produto.component';
-import { TabelaDoProdutorComponent } from './tabela-do-produtor/tabela-do-produtor.component';
-import { AdminComponent } from './admin/admin.component';
-import { ProdutorComponent } from './produtor-logado/produtor-logado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, PortfolioItemComponent, CadastroFormComponent, TabelaDoProdutorComponent, AdminComponent, ProdutorComponent ],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Produtos-Locais-Sobradinho';
+
 }
